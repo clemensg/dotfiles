b41f6b9576777292e1d591751bc064bbfabef243
Clemens Gruber <clemensgru@gmail.com>
2013-09-17 17:14:13 +0200

    Improvements: emacsclient, rvm

--- zsh/os/osx.zsh
+++ zsh/os/osx.zsh
@@ -1,11 +1,18 @@
 # Mac OS X specific zsh configuration
 #
 
-# Emacs aliases
-export EDITOR="/Applications/Emacs.app/Contents/MacOS/Emacs -nw"
-alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs -nw"
-alias emacsgui="/Applications/Emacs.app/Contents/MacOS/Emacs"
+# Homebrew
+if [[ -d "$HOME/homebrew" ]]; then
+  export HOMEBREW_DEVELOPER=1
+  export HOMEBREW_PREFIX=$HOME/homebrew
+fi
 
 # Postgres aliases
 alias start_postgres="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
 alias stop_postgres="pg_ctl -D /usr/local/var/postgres stop -s -m fast"
+
+# RVM
+if [[ -d "$HOME/.rvm" ]]; then
+  PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+  source $HOME/.rvm/scripts/rvm
+fi
