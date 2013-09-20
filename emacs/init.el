fe097d45837325bb42c25fe2919a68499c080ebb
Clemens Gruber <clemensgru@gmail.com>
2013-09-20 21:37:37 +0200

    Linux compatibility

--- emacs/init.el
+++ emacs/init.el
@@ -26,7 +26,7 @@
 
 ; My packages
 (setq my-required-packages
-      (list 'flycheck 'inf-ruby 'rvm))
+      (list 'flycheck 'inf-ruby 'rvm 'solarized-theme))
 (dolist (package my-required-packages)
   (when (not (package-installed-p package))
     (package-refresh-contents)
@@ -58,26 +58,20 @@
 (setq-default save-place t)
 (setq save-place-file "~/.emacs.d/saved-places")
 
-;; In case we are in a GUI
-(set-face-attribute
-  'default nil :font "Menlo 16")
-(when (display-graphic-p)
-  (load-theme 'solarized-light))
-
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
- '(safe-local-variable-values (quote ((encoding . utf-8)))))
-
+ '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; In case we are in a GUI
+(when (display-graphic-p)
+  (set-face-attribute 'default nil :font "Inconsolata 12")
+  (load-theme 'solarized-light))
